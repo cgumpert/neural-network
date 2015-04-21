47d80c0bd5499af4326a63425f889e589a602b5e
Christian Gumpert <cgumpert@cern.ch>
2015-04-21 17:00:59 +0200

    added script for checking gradients

---
+++ checkNNGradients.m
@@ -0,0 +1,41 @@
+function checkNNGradients(weights,arch,X,y,lambda)
+%CHECKNNGRADIENTS Creates a small neural network to check the
+%backpropagation gradients
+%   CHECKNNGRADIENTS(lambda) Creates a small neural network to check the
+%   backpropagation gradients, it will output the analytical gradients
+%   produced by your backprop code and the numerical gradients (computed
+%   using computeNumericalGradient). These two gradient computations should
+%   result in very similar values.
+%
+
+[cost, grad] = nnCostFunctionReg(weights,arch,X,y,lambda)
+numgrad = zeros(size(weights));
+perturb = zeros(size(weights));
+e = 1e-4;
+for p = 1:numel(weights)
+    % Set perturbation vector
+    perturb(p) = e;
+    p
+    loss1 = nnCostFunctionReg(weights - perturb,arch,X,y,lambda);
+    loss2 = nnCostFunctionReg(weights + perturb,arch,X,y,lambda);
+    % Compute Numerical Gradient
+    numgrad(p) = (loss2 - loss1) / (2*e);
+    perturb(p) = 0;
+end
+
+% Visually examine the two gradient computations.  The two columns
+% you get should be very similar. 
+disp([numgrad grad]);
+fprintf(['The above two columns you get should be very similar.\n' ...
+         '(Left-Your Numerical Gradient, Right-Analytical Gradient)\n\n']);
+
+% Evaluate the norm of the difference between two solutions.  
+% If you have a correct implementation, and assuming you used EPSILON = 0.0001 
+% in computeNumericalGradient.m, then diff below should be less than 1e-9
+diff = norm(numgrad-grad)/norm(numgrad+grad);
+
+fprintf(['If your backpropagation implementation is correct, then \n' ...
+         'the relative difference will be small (less than 1e-9). \n' ...
+         '\nRelative Difference: %g\n'], diff);
+
+end
