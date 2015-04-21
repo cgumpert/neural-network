2cdcae7f945894c182210ee0ac7bfa35bfa2432b
cgumpert <cgumpert@users.noreply.github.com>
2015-04-21 20:49:11 +0200

    update run script

--- run_nn.m
+++ run_nn.m
@@ -4,19 +4,37 @@
 # - last layer is the output layer
 # - bias nodes should not be included
 
-arch = [400,25,10];
+data = load('sherpa_nn_test.dat');
+X = data(:,1:18);
+y = data(:,21);
+y = log(y);
+y = (y - min(y))./(max(y) - min(y));
 
-load('../machine-learning-ex4/ex4/ex4data1.mat');
+% split into training and test set
+train_size = 0.5;
+train_idx = floor(train_size * size(X,1));
+sel = randperm(size(X, 1));
+X_train = X(sel(1:train_idx),:);
+y_train = y(sel(1:train_idx),:);
+X_test = X(sel(train_idx+1:end),:);
+y_test = y(sel(train_idx+1:end),:);
+
+arch = [18,25,1];
+
+[nn cost] = trainNetwork(X_train,y_train,arch,1);
+fprintf("training cost: %.3f\n",cost);
+y_pred = predict(arch,nn,X_test);
+fprintf("test cost: %.3f",nnCostFunction(nn,arch,X_test,y_test));
+
+break
+load('../../coursera/ML/machine-learning-ex4/ex4/ex4data1.mat');
+load('../../coursera/ML/machine-learning-ex4/ex4/ex4weights.mat');
 m = size(X, 1);
 y_vec = zeros(m,10);
 for i = 1:m
   y_vec(i,y(i)) = 1;
 end
 
-
-% Randomly select 100 data points to display
-sel = randperm(size(X, 1));
-sel = sel(1:100);
 
 displayData(X(sel, :));
 pause;
