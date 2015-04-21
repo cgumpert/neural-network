bd331b5975f010b01f3a5782c57ba701ca53164f
Christian Gumpert <cgumpert@cern.ch>
2015-04-21 17:00:35 +0200

    make run script work

--- run_nn.m
+++ run_nn.m
@@ -6,13 +6,13 @@
 
 arch = [400,25,10];
 
-load('../../coursera/ML/machine-learning-ex4/ex4/ex4data1.mat');
+load('../machine-learning-ex4/ex4/ex4data1.mat');
 m = size(X, 1);
-y_vec = zeros(10,m);
+y_vec = zeros(m,10);
 for i = 1:m
-    y_vec(i,y(i)) = 1;
+  y_vec(i,y(i)) = 1;
 end
-y = y_vec;
+
 
 % Randomly select 100 data points to display
 sel = randperm(size(X, 1));
@@ -21,8 +21,9 @@ sel = sel(1:100);
 displayData(X(sel, :));
 pause;
 
-[nn cost] = trainNetwork(X,y,arch,0);
+[nn cost] = trainNetwork(X,y_vec,arch,1);
 
 pred = predict(arch,nn,X);
 
-fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+[max_val,y_pred] = max(pred,[],2) ;
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(y_pred == y)) * 100);
